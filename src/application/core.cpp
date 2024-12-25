3a9f1e4f6ae3bec9e8de364d211f97523990c171
MrsRina <diynggame@gmail.com>
2024-12-25 14:32:28 -0300

    [build][tweaks] initial cmake build, cmake vars tweaks

---
+++ src/application/core.cpp
@@ -0,0 +1,8 @@
+#include "application/core.hpp"
+#include <iostream>
+
+int32_t main(int32_t, char**) {
+  std::cout << "meow" << std::endl;
+
+  return 1;
+}
---
+++ src/application/core.hpp
@@ -0,0 +1,11 @@
+#ifndef APPLICATION_CORE_HPP
+#define APPLICATION_CORE_HPP
+
+namespace application {
+  struct context {
+  public:
+
+  };
+}
+
+#endif
